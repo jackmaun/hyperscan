7aa6b2d6a082331b9c18bb1dca10da0201028ef2
Jack Maunsell <jackmaun@colostate.edu>
2025-06-10 09:54:40 -0600

    added more secrets

--- scanners/memory.go
+++ scanners/memory.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"path/filepath"
+
 	"github.com/jackmaun/hyperscan/extractors"
 	"github.com/edsrzf/mmap-go"
 )
 
 var patterns = map[string]*regexp.Regexp{
-	"AWS Access Key": regexp.MustCompile(`AKIA[0-9A-Z]{16}`),
-	"JWT Token":      regexp.MustCompile(`eyJ[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+`),
-	"Password Key":   regexp.MustCompile(`(?i)password\s*=\s*[^\s"]{4,}`),
-	"NTLM Hash":      regexp.MustCompile(`[a-fA-F0-9]{32}:[a-fA-F0-9]{32}`), // placeholder for now
+	"AWS Access Key":            regexp.MustCompile(`AKIA[0-9A-Z]{16}`),
+	"JWT":     	             regexp.MustCompile(`eyJ[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+`),
+	"Password Key":              regexp.MustCompile(`(?i)password\s*=\s*[^\s"]{4,}`),
+	"NTLM Hash":                 regexp.MustCompile(`[a-fA-F0-9]{32}:[a-fA-F0-9]{32}`),
+	"NTLMv2 Hash":               regexp.MustCompile(`[a-zA-Z0-9_.\\-]+::[a-zA-Z0-9_.\\-]+:[a-fA-F0-9]{16}:[a-fA-F0-9]{32,256}:.+`),
+	"NetNTLMv2 Challenge":       regexp.MustCompile(`[^\s:]+::[^\s:]+:[a-fA-F0-9]{16}:[a-fA-F0-9]{32,}`),
+	"LM:NTLM Hash Pair":         regexp.MustCompile(`[a-fA-F0-9]{32}:[a-fA-F0-9]{32}`),
+	"Kerberos KRB-CRED":         regexp.MustCompile(`KRB-CRED`),
+	"Kerberos Ticket ASN.1":     regexp.MustCompile(`\x6e\x82[\x00-\xff]{2}\x30\x82`),
+	"Kerberos Base64 Ticket":    regexp.MustCompile(`(?:YII|doIF)[A-Za-z0-9+/=]{100,}`),
+	"DPAPI GUID":                regexp.MustCompile(`(?i)\{[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\}`),
+	"DPAPI Blob":	             regexp.MustCompile(`(?s)\x01\x00\x00\x00.{80,700}`),
 }
 
 func ScanMemory(path string, outDir string) error {
@@ -29,7 +39,7 @@ func ScanMemory(path string, outDir string) error {
 	}
 	defer mmapData.Unmap()
 
-	fmt.Printf("Scanning memory file (size: %d bytes)...\n", len(mmapData))
+	fmt.Printf("Scanning memory file (%s, size: %d bytes)...\n", filepath.Base(path), len(mmapData))
 
 	for name, re := range patterns {
 		matches := re.FindAll(mmapData, -1)
